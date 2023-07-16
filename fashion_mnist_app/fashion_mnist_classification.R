8a35434f5be1e7309db10c2eaeea40eb28b20825
Back Young-Rae <larryback@naver.com>
2023-07-16 19:04:38 +0900

    Update fashion_mnist_classification.R
    updated fashion_mnist_classification.R

--- fashion_mnist_app/fashion_mnist_classification.R
+++ fashion_mnist_app/fashion_mnist_classification.R
@@ -1,8 +1,3 @@
-setwd("C:/R/R_folder/app_folder-2/fashion_mnist_app")
-getwd()
-
-
-
 # Load required libraries
 library(shiny)
 library(ggplot2)
@@ -100,16 +95,4 @@ ui <- fluidPage(
 shinyApp(ui = ui, server = server)
 
 
-# Add deployment configuration
-rsconnect::setAccountInfo(name='insightfulapps-for-everyone',
-                          token='16999D937A4EE185888CE7751C7E1B71',
-                          secret='kusXTkNfLTZ/kHdOS5TxsQrgBH5Td4o5P5t3vZM2')
-
-
-#rsconnect::deployApp("C:/R/R_folder/app_folder-2/fashion_mnist_app")
-
-#rsconnect::showLogs("C:/R/R_folder/app_folder-2/fashion_mnist_app")
-
-#library(tools)
-#file_path_as_absolute(fashion_mnist_classification.R)
 
